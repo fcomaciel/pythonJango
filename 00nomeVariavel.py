dcabfb34850f603bca7678ef5ecbbe24228b41e0
Francisco Maciel <fcomaciel@gmail.com>
2016-12-12 19:59:35 -0200

    Dois Scripts adicionados...

---
+++ 00nomeVariavel.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+'''
+0. Exercício nomes de variáveis
+============================
+Escreava um programa que calcule as horas trabalhada de um Freelance
+e imprima na tela o total a ser pago. Base para cálculo:
+Horas trabalhada - 35
+Valor da hora trabalhada: R$ 12.50
+'''
+
+a = 35.0 # Horas
+b = 12.5 # Valor hora trabalhada
+c = a * b # Valor pagamento
+
+print(c)
+
---
+++ 01nomeVariaisMnemonicos.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+'''
+1. Exercício nomes de variáveis Mnemônicos
+=======================================
+Escreava um programa que calcule as horas trabalhada de um Freelance
+e imprima na tela o total a ser pago.
+
+Base para cálculo:
+Horas trabalhada - 35
+Valor da hora trabalhada: R$ 12.50
+'''
+
+horasTrabalhada = 35.0                              # Horas
+valorPorHora = 12.5                                 # Valor hora trabalhada
+totalPagamento = horasTrabalhada * valorPorHora     # Valor pagamento
+
+print(totalPagamento)
+
+
+
